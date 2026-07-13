0ec4f5520da638eff1f71adce87681edf9425844
VanThanhdz <hvtt22512@gmail.com>
2026-07-13 15:00:39 +0700

    Thanhdz
    tach_file_css

--- Velora/VeloraVue/src/components/ThemeToggle.vue
+++ Velora/VeloraVue/src/components/ThemeToggle.vue
@@ -40,56 +40,5 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.theme-toggle-wrapper {
-  cursor: pointer;
-  display: inline-block;
-}
-
-/* Thanh ray trượt */
-.toggle-track {
-  width: 54px;
-  height: 28px;
-  background-color: #e0e0e0; /* Màu nền lúc trắng */
-  border-radius: 30px;
-  position: relative;
-  transition: all 0.4s ease;
-  border: 1px solid #ccc;
-}
-
-/* Thanh ray trượt lúc sang nền Nâu */
-.toggle-track.is-brown {
-  background-color: #24201D;
-  border-color: #d1aa68;
-}
-
-/* Cục tròn trượt qua lại */
-.toggle-thumb {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  width: 22px;
-  height: 22px;
-  background-color: #ffffff;
-  border-radius: 50%;
-  transition: all 0.4s cubic-bezier(0.68, -0.55, 0.265, 1.55);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 0 2px 5px rgba(0,0,0,0.2);
-}
-
-.toggle-track.is-brown .toggle-thumb {
-  transform: translateX(26px);
-  background-color: #d1aa68; /* Cục tròn màu vàng kim lúc nền Nâu */
-}
-
-/* Icon bên trong */
-.icon-sun {
-  color: #f39c12;
-  font-size: 12px;
-}
-.icon-moon {
-  color: #24201D;
-  font-size: 12px;
-}
+@import "./CSS/ThemeToggle.css";
 </style>
